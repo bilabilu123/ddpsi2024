0dc6255a6a275f0c46fc03e1cfaf9374cc8606bd
bilabilu123 <ininabilaa1@gmail.com>
2024-11-19 20:39:00 -1200

    Delete ddp9_1.py

--- ddp9_1.py
+++
@@ -1,7 +0,0 @@
-print("--- mencari celcius ke fahrenheit ---")
-def celcius_ke_fahrenheit(celcius):
-    fahrenheit = (celcius * 9/5) + 32
-    return fahrenheit
-
-print(celcius_ke_fahrenheit(0))
-print(celcius_ke_fahrenheit(100))
