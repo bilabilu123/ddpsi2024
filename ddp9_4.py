5496cf4978f697f70de09304023bbc6e6128096f
bilabilu123 <ininabilaa1@gmail.com>
2024-11-19 20:39:42 -1200

    Delete ddp9_4.py

--- ddp9_4.py
+++
@@ -1,9 +0,0 @@
-print('\n--- Cetak Bilangan Ganjil ---')
-
-def bil_ganjil(angka):
-    for i in range(1, angka, 2):
-        print(i, end= " ") #end agar outputnya berurut kesamping
-
-#untuk mencetak velue
-bil_ganjil(20)
-    
