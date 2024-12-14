a12ea73c1e6de8806313d5770bb6397c33e219e3
bilabilu123 <ininabilaa1@gmail.com>
2024-12-13 16:19:58 -1200

    Add files via upload
    TugasDDP9_Nabila Anggraini_0110124044_SI12

---
+++ ddp9_4.py
@@ -0,0 +1,9 @@
+print('\n--- Cetak Bilangan Ganjil ---')
+
+def bil_ganjil(angka):
+    for i in range(1, angka, 2):
+        print(i, end= " ") #end agar outputnya berurut kesamping
+
+#untuk mencetak velue
+bil_ganjil(20)
+    
---
+++ dpp9_2.py
@@ -0,0 +1,7 @@
+print('\n--- Mencari bilangan genap ---')
+def is_genap(bil_genap):
+    return bil_genap %2 == 0
+
+#untuk maencetak velue
+print(is_genap(4))
+print(is_genap(7))
